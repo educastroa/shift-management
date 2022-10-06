a533286b768d4a711b3d57930a501909b557b2ef
Eduardo Castro <eduardocastro.a@hotmail.com>
2022-10-06 18:06:12 -0400

    add logo, install sass and add style to login component

---
+++ client/src/components/Login/Login.tsx
@@ -0,0 +1,54 @@
+import React, { useState, ChangeEvent } from "react";
+import styles from "./Login.module.scss";
+import logo from "../../assets/javascript-logo.png"
+import logo1 from "../../assets/company-logo.jpg"
+
+
+interface Inputs {
+  email?: string;
+  password?: string;
+}
+
+function Login() {
+  const [inputs, setInputs] = useState<Inputs>();
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target as HTMLInputElement;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  
+
+  return (
+    <div className={styles.loginContainer}>
+      <div>
+        <div >
+        <img src={logo1} alt='logo'/>
+        </div>
+        <form >
+          <div className={styles.formInput}>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              required
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.formInput}>
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              required
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
